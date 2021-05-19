3ed062f75aaca1790cad90f942f8728665f0e451
solchan87 <perudot@gmail.com>
2021-05-19 20:21:02 +0900

    CleanArchitecture + MVVM

---
+++ CleanMVVM/Sources/Application/Scenes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanMVVM
+//
+//  Created by Tom on 2021/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
